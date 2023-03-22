dc3468b7a45ed1f45b04ef45461bd5d207a66a28
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 20:24:51 +0000

    refactored into class method checkProtein

--- src/DNA.java
+++ src/DNA.java
@@ -3,6 +3,18 @@ public class DNA {
      * 1. Must begin with a 'start codon' : ATG
      * 2. Must end with a 'stop codon': TGA
      * 3 In between, each aditional codon is a sequence of three nucleotides */
+
+    public void checkProtein(String dna) {
+        int start = dna.indexOf("ATG");
+        int stop = dna.indexOf("TGA");
+
+        if (start >= 0 && stop >= 0 && (stop - start) % 3 == 0) {
+            String protein = dna.substring(start, stop + 3);
+            System.out.println("Protein: " + protein);
+        } else {
+            System.out.println("No protein");
+        }
+    }
     public static void main(String[] args) {
 
         //  -. .-.   .-. .-.   .
@@ -13,16 +25,16 @@ public class DNA {
         // declare and assigned 3 Sting variables for dna strings and one to combine all 3.
         String dna1 ="ATGCGATACGCTTGA", dna2 = "ATGCGATACGTGA", dna3 = "ATTAATATGTACTGA", dna = dna1 + dna2 + dna3;
 
-        System.out.println(dna.length());
-        int condonStart = dna.indexOf("ATG");
-        int condonEnd = dna.indexOf("TGA");
+        DNA newDNA = new DNA();
+        newDNA.checkProtein(dna1);
+        newDNA.checkProtein(dna2);
+        newDNA.checkProtein(dna3);
 
-        if (condonStart != -1 && condonEnd != -1 && (condonEnd - condonStart) % 3 == 0) {
-            String protein = dna.substring(condonStart, condonEnd + 3);
-            System.out.println("Protein: " + protein);
-        } else {
-            System.out.println("No protein");
-        }
+
+
+
+
+
 
 
 
