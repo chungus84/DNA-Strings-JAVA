a11c900be06de88baed11ac1190434651ebd5cbb
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 19:39:08 +0000

    Add 3 dna(1,2,3) variables and one dna variable which combines all 3

--- src/DNA.java
+++ src/DNA.java
@@ -1,11 +1,19 @@
 public class DNA {
-
+    /** Program to determine if a strain of DNA is protein or not
+     * 1. Must begin with a 'start codon' : ATG
+     * 2. Must end with a 'stop codon': TGA
+     * 3 In between, each aditional codon is a sequence of three nucleotides */
     public static void main(String[] args) {
 
         //  -. .-.   .-. .-.   .
         //    \   \ /   \   \ /
         //   / \   \   / \   \
         //  ~   `-~ `-`   `-~ `-
+
+        // declare and assigned 3 Sting variables for dna strings and one to combine all 3.
+        String dna1 ="ATGCGATACGCTTGA", dna2 = "ATGCGATACGTGA", dna3 = "ATTAATATGTACTGA", dna = dna1 + dna2 + dna3;
+
+        System.out.println(dna.length());
 
 
 
