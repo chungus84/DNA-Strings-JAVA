ff5c943301b4a36b8ae403c5e68cfb0b8f58c323
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 19:29:11 +0000

    Initial project setup

---
+++ src/DNA.java
@@ -0,0 +1,14 @@
+public class DNA {
+
+    public static void main(String[] args) {
+
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ /
+        //   / \   \   / \   \
+        //  ~   `-~ `-`   `-~ `-
+
+
+
+
+    }
+}
